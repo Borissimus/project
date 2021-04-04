d056549ea47d0ece3e6e73c2a367f60a099c3f90
Borissimus <borysworking@gmail.com>
2021-04-05 00:17:31 +0300

    added task01.c

---
+++ task01.c
@@ -0,0 +1,21 @@
+/*
+filename task01.c, author Borissimus, tool emacs(GIU), OS Linux (Ubuntu18.04);
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int num = 0;
+    printf("Please, type an integer number then press key \'Enter\'\n\r");
+    scanf("%d",&num);
+    printf("%d\n\r", num * num);
+    return 0;
+}
+
+/*
+formulas:
+Uo = Ui * R1 / (R2 + R1);
+Ui = Uo*(R2 + R1) / R1;
+R1 = 
+R2 = 
+*/
